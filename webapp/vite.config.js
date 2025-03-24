57bba6be30b5083b96b50f3eeb154e3ca3d97993
NurzhanMukhitov <n.mukhitov@larnilane.com>
2025-03-24 21:35:00 +0100

    feat: integrate webapp into main repository

---
+++ webapp/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/asist_bro/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: true,
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+}) 
